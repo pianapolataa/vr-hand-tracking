dcc1a5cc639be5e874cbd690a95a15f8a117c9d3
joliachen <zc2745@nyu.edu>
2025-08-24 10:04:13 -0400

    delete useless file

--- franka-env/franka_env/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from franka_env.envs.franka_env import FrankaEnv
-from franka_env.envs.franka_env_relative import FrankaEnvRelative
-
-__all__ = ["FrankaEnv", "FrankaEnvRelative"]
--- franka-env/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="franka_env",
-    version="0.0.1",
-    packages=["franka_env"],
-    install_requires=["gym"],
-)
